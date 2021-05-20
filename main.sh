d46f73cd05a289e8d408f07586fcc1c8a0f72aec
Samuel Gerike <>
2021-05-20 00:29:50 +0000

    1st push

--- main.sh
+++ main.sh
@@ -1,6 +1,8 @@
-echo Please Wait....
-cp -r gemBINS ../.gem
+echo Please Wait...
+mkdir ../.gem
+cp -r gemBINS/* ../.gem
 export PATH=/home/runner/.gem/ruby/2.5.0/bin:$PATH
+chmod +x /home/runner/.gem/ruby/2.5.0/bin/*
 jekyll -v
 cd site
 jekyll serve -P 8080 --host=0.0.0.0
